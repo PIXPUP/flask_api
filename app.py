817f5855252f865c93a5ba60b7de03385c990a51
sean <pupjaa130@gmail.com>
2024-02-26 14:01:19 +0700

    #

--- app.py
+++ app.py
@@ -20,35 +20,35 @@ def register():
          return 'username was taken'
 @app.route("/login",methods=['POST'])
 def login():
-            try:
-                j_req = request.json
-                username = j_req['Username']
-                password = j_req['Password']
-                user = database_system.find_data(username)
-                if check_password_hash(user.Password,user.Salt+password):
-                    return f'Hello, {username}'
-                else:
-                    return 'Password is incorrect'
-            except:
-                return 'username is not found'
+    try:
+        j_req = request.json
+        username = j_req['Username']
+        password = j_req['Password']
+        user = database_system.find_data(username)
+        if check_password_hash(user.Password,user.Salt+password):
+            return f'Hello, {username}'
+        else:
+            return 'Password is incorrect'
+    except:
+        return 'username is not found'
 
 @app.route("/ChangePassword",methods=['PUT'])
 def ChangePassword():
-        try:
-            j_req = request.json
-            username = j_req['Username']
-            old_password = j_req['Old Password']
-            user = database_system.find_data(username)
-            new_password = generate_password_hash(user.Salt+j_req['New Password'])
-            new_user={"Username":user.Username, "Password":new_password, "Email":user.Email}
-            j_req = request.get_json()
-            if check_password_hash(user.Password,user.Salt+old_password):
-                database_system.updata_data(username,new_user)
-                return 'change password success'
-            else:
-                return 'password is incorrect'
-        except:
-            return 'username is not found'
+    try:
+        j_req = request.json
+        username = j_req['Username']
+        old_password = j_req['Old Password']
+        user = database_system.find_data(username)
+        new_password = generate_password_hash(user.Salt+j_req['New Password'])
+        new_user={"Username":user.Username, "Password":new_password, "Email":user.Email}
+        j_req = request.get_json()
+        if check_password_hash(user.Password,user.Salt+old_password):
+            database_system.updata_data(username,new_user)
+            return 'change password success'
+        else:
+            return 'password is incorrect'
+    except:
+        return 'username is not found'
 
 @app.route("/DeleteAccount",methods=['DELETE'])
 def delete_account():
